151a33e42cbb7ffbb1e218d1f887a7620432a262
Jasper Ballantyne <jasper.ballantyne@gmail.com>
2020-04-16 07:56:54 +1200

    setup navigation

--- app/src/main/java/com/example/randomuser/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/randomuser/ui/main/MainFragment.kt
@@ -1,18 +1,14 @@
 package com.example.randomuser.ui.main
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.example.randomuser.R
 
 class MainFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = MainFragment()
-    }
 
     private lateinit var viewModel: MainViewModel
 
@@ -23,7 +19,7 @@ class MainFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
     }
 
