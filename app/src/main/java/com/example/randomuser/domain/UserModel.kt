5767c1782be5c938fa49a28b361fe6826ffee972
Jasper Ballantyne <jasper.ballantyne@gmail.com>
2020-04-16 16:27:13 +1200

    create initial domain model

---
+++ app/src/main/java/com/example/randomuser/domain/UserModel.kt
@@ -0,0 +1,12 @@
+package com.example.randomuser.domain
+
+data class UserModel(
+    val uuid: String,
+    val title: String,
+    val firstName: String,
+    val LastName: String,
+    val gender: String,
+    val dateOfBirth: String,
+    val thumbnailUrl: String,
+    val profilePhotoUrl: String
+)
