664c234fde64d56ac4a9a15d5d279f66e788add5
Jasper Ballantyne <jasper.ballantyne@gmail.com>
2020-04-18 07:37:11 +1200

    update service and DTO (working)

--- app/src/main/java/com/example/randomuser/network/DataTransferObjects.kt
+++ app/src/main/java/com/example/randomuser/network/DataTransferObjects.kt
@@ -1,9 +1,10 @@
 package com.example.randomuser.network
 
+import com.example.randomuser.database.DatabaseUser
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
-data class NetworkUserContainer(val videos: List<NetworkUser>)
+data class NetworkUserContainer(val results: List<NetworkUser>)
 
 @JsonClass(generateAdapter = true)
 data class NetworkUser(
@@ -12,16 +13,16 @@ data class NetworkUser(
     val location: Location,
     val email: String,
     val login: Login,
-    val dateOfBirth: DateOfBirth,
+    val dob: DateOfBirth,
     val phone: String,
-    val profilePhotoUrl: Picture
+    val picture: Picture
 )
 
 @JsonClass(generateAdapter = true)
 data class Name(
     val title: String,
-    val firstName: String,
-    val lastName: String
+    val first: String,
+    val last: String
 )
 
 @JsonClass(generateAdapter = true)
@@ -43,6 +44,20 @@ data class DateOfBirth(
 
 @JsonClass(generateAdapter = true)
 data class Picture(
-    val profilePhotoUrl: String
+    val large: String
 )
+
+fun NetworkUserContainer.asDatabaseModel(): List<DatabaseUser> {
+    return results.map {
+        DatabaseUser(
+            uuid = it.login.uuid,
+            title = it.name.title,
+            firstName = it.name.first,
+            lastName = it.name.last,
+            gender = it.gender,
+            dateOfBirth = it.dob.date,
+            profilePhotoUrl = it.picture.large
+        )
+    }
+}
 
--- app/src/main/java/com/example/randomuser/network/Service.kt
+++ app/src/main/java/com/example/randomuser/network/Service.kt
@@ -1,22 +1,28 @@
 package com.example.randomuser.network
 
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import kotlinx.coroutines.Deferred
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 interface UserService {
-    @GET("?page=3&results=5&exc=registered,cell,id,nat")
+    @GET("?page=3&results=5&exc=registered,cell,id,nat&noinfo")
     fun getUsers(): Deferred<NetworkUserContainer>
 }
+
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
 
 object UserNetwork {
     private val retrofit = Retrofit.Builder()
         .baseUrl("https://randomuser.me/api/")
-        .addConverterFactory(MoshiConverterFactory.create())
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
         .addCallAdapterFactory(CoroutineCallAdapterFactory())
         .build()
 
-    val users = retrofit.create(UserService::class.java)
+    val retrofitService : UserService by lazy { retrofit.create(UserService::class.java) }
 }
