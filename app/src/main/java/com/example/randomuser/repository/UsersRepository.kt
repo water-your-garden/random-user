5860848a0afb029b5fc355a0d569be8b96a86cd7
Jasper Ballantyne <jasper.ballantyne@gmail.com>
2020-04-18 07:38:13 +1200

    setup repository

---
+++ app/src/main/java/com/example/randomuser/repository/UsersRepository.kt
@@ -0,0 +1,25 @@
+package com.example.randomuser.repository
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
+import com.example.randomuser.database.UserDatabase
+import com.example.randomuser.database.asDomainModel
+import com.example.randomuser.domain.UserModel
+import com.example.randomuser.network.UserNetwork
+import com.example.randomuser.network.asDatabaseModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class UsersRepository(private val database: UserDatabase) {
+
+    val users: LiveData<List<UserModel>> = Transformations.map(database.userDao.getUsers()) {
+        it.asDomainModel()
+    }
+
+    suspend fun refreshUsers() {
+        withContext(Dispatchers.IO) {
+            val userList = UserNetwork.retrofitService.getUsers().await()
+            database.userDao.insertAll(userList.asDatabaseModel())
+        }
+    }
+}
